b6c0a5276f49d404ac152183a0f3ba5ea58117a4
takshsuri5-cmd <takshsuri5@gmail.com>
2025-11-21 22:16:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -249,8 +249,8 @@ div[role=radiogroup] > label:active {{
 .stRadio > div {{
     display: inline-flex !important;
     align-items: center !important;
-    margin-top: 6px !important; 
-    margin-left: -187px !important;   /* pulls it closer to label */
+    margin-top: 3px !important; 
+    margin-left: -192px !important;   /* pulls it closer to label */
 }}
 
 .stRadio label {{
@@ -790,6 +790,7 @@ st.markdown("""
     © Hisaab Kitab
 </div>
 """, unsafe_allow_html=True)
+
 
 
 
